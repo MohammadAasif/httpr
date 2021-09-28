7a85662edbe075cc3c2e9bff9eabe7c162a8e85d
MohammadAasif <89965648+MohammadAasif@users.noreply.github.com>
2021-09-28 14:54:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Http from "./http";
+import React, { Component } from "react";
+
+function App() {
+    return (
+        <div className="App">
+            <Http />
+        </div>
+    );
+}
+
+
+
+export default App;
